4edb7a079e5598c65bea03b57c856c7fa8bad77d
AffanSurya <affanqwe04@gmail.com>
2024-05-08 18:46:38 +0700

    mengubah tipe data price di tabel dan menyelesaikan halaman show menu1

--- database/migrations/2024_05_05_132317_create_menu_items_2_table.php
+++ database/migrations/2024_05_05_132317_create_menu_items_2_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->decimal('price');
+            $table->integer('price');
             $table->string('category');
             $table->string('image');
             $table->string('status')->default('available');
