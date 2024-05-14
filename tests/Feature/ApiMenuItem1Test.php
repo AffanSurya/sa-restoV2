23d01f3aa38e939e1cab5d1980189479f1bc5899
AffanSurya <affanqwe04@gmail.com>
2024-05-15 01:12:22 +0700

    api menu item 1 test selesai

--- tests/Feature/ApiMenuItem1Test.php
+++ tests/Feature/ApiMenuItem1Test.php
@@ -1,40 +1,34 @@
 <?php
 
 use App\Models\MenuItem1;
-use App\Models\User;
-
-beforeEach(function () {
-    $this->user = User::findOrFail(1);
-});
-
-test('example', function () {
-    $response = $this->actingAs($this->user)->get('/');
-
-    $response->assertStatus(200);
-});
 
 test('menampilkan data', function () {
-    $response = $this->actingAs($this->user)->get('/menuItem1');
+    $response = $this->get('/api/menuItem1');
 
     $response->assertStatus(200);
+
+    expect($response->getContent())->toBeJson();
 });
 
 test('menambahkan data', function () {
     $menuItem1 = MenuItem1::factory()->create();
 
-    $response = $this->actingAs($this->user)->post('/menuItem1/store', $menuItem1->toArray());
-    $response->assertStatus(302);
+    $response = $this->post('/api/menuItem1/store', $menuItem1->toArray());
+    $response->assertStatus(201);
 
     $this->assertDatabaseHas('menu_items_1', $menuItem1->toArray());
 
+    expect($response->json())->toHaveKey('message');
     $menuItem1->delete();
 });
 
-test('menampilkan 1 data untuk di edit', function () {
+test('menampilkan 1 data ', function () {
     $menuItem1 = MenuItem1::factory()->create();
 
-    $response = $this->actingAs($this->user)->get("/menuItem1/edit/{$menuItem1->id}");
+    $response = $this->get("/api/menuItem1/show/{$menuItem1->id}");
     $response->assertStatus(200);
+
+    expect($response->getContent())->toBeJson();
 
     $menuItem1->delete();
 });
@@ -50,10 +44,12 @@ test('mengubah data', function () {
         'image' => 'https://www.blibli.com/friends-backend/wp-content/uploads/2022/11/24b5469496e15e10897db2e8778d906e.jpg',
     ];
 
-    $response = $this->actingAs($this->user)->put("/menuItem1/update/{$menuItem1->id}", $updateMenuItem1);
-    $response->assertStatus(302);
+    $response = $this->put("/api/menuItem1/update/{$menuItem1->id}", $updateMenuItem1);
+    $response->assertStatus(200);
 
     $this->assertDatabaseHas('menu_items_1', $updateMenuItem1);
+
+    expect($response->json())->toHaveKey('message');
 
     $menuItem1->delete();
 });
@@ -61,10 +57,12 @@ test('mengubah data', function () {
 test('menghapus data', function () {
     $menuItem1 = MenuItem1::factory()->create();
 
-    $response = $this->actingAs($this->user)->delete("/menuItem1/delete/{$menuItem1->id}");
-    $response->assertStatus(302);
+    $response = $this->delete("/api/menuItem1/delete/{$menuItem1->id}");
+    $response->assertStatus(200);
 
     $this->assertDatabaseMissing('menu_items_1', $menuItem1->toArray());
+
+    expect($response->json())->toHaveKey('message');
 
     $menuItem1->delete();
 });
